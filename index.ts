f3e77431ebda155fde3fcbb26f568c6f72e00ce9
Eetu <datmixor@gmail.com>
2020-07-13 18:28:41 +0300

    Created basic web server that delivers static files along with an api

--- index.ts
+++ index.ts
@@ -1,16 +1,28 @@
 import { Application } from "https://deno.land/x/abc@v1/mod.ts"
+import { HttpMethod } from 'https://deno.land/x/abc@v1/constants.ts'
+import { CORSConfig, cors } from 'https://deno.land/x/abc@v1/middleware/cors.ts'
+//import Deno from 'deno'
+
+
+const config: CORSConfig = {
+  allowOrigins: ['*'],
+  allowMethods: [HttpMethod.Get]
+};
+
 
 const app = new Application();
 
-const port: number = 8080;
+const port: number = 8000;
 
 app
   .get('/api', c => {
-	return 'api';
+	const content = Deno.readTextFile('./testcontent.json').then(data => JSON.parse(data));
+	return content;
   })
   .file('/', 'dist/index.html')
   .static('/static', '/dist/static')
   .start({ port: port });
 
+app.use(cors(config))
 
 console.log(`Running on localhost:${port}`)
